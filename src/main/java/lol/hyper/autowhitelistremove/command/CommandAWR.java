43ea05f452ad73352609687b5a486df0dd0ba8e7
hyperdefined <contact@hyper.lol>
2021-06-14 12:03:48 -0400

    forgot to add these

--- src/main/java/lol/hyper/autowhitelistremove/command/CommandAWR.java
+++ src/main/java/lol/hyper/autowhitelistremove/command/CommandAWR.java
@@ -23,7 +23,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -84,6 +84,6 @@ public class CommandAWR implements TabExecutor {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        return Collections.singletonList("check");
+        return Arrays.asList("check", "help", "reload");
     }
 }

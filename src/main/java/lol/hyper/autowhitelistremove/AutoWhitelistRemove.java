3e30eabadcf3efcda887cabb576044c6f1d991d0
hyperdefined <contact@hyper.lol>
2021-12-02 12:42:47 -0500

    1.1.1

--- src/main/java/lol/hyper/autowhitelistremove/AutoWhitelistRemove.java
+++ src/main/java/lol/hyper/autowhitelistremove/AutoWhitelistRemove.java
@@ -18,8 +18,9 @@
 package lol.hyper.autowhitelistremove;
 
 import lol.hyper.autowhitelistremove.command.CommandAWR;
-import lol.hyper.autowhitelistremove.tools.UpdateChecker;
 import lol.hyper.autowhitelistremove.tools.WhitelistCheck;
+import lol.hyper.githubreleaseapi.GitHubRelease;
+import lol.hyper.githubreleaseapi.GitHubReleaseAPI;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -27,6 +28,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 public final class AutoWhitelistRemove extends JavaPlugin {
@@ -50,13 +52,7 @@ public final class AutoWhitelistRemove extends JavaPlugin {
 
         new Metrics(this, 11684);
 
-        new UpdateChecker(this, 93326).getVersion(version -> {
-            if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
-                logger.info("You are running the latest version.");
-            } else {
-                logger.info("There is a new version available! Please download at https://www.spigotmc.org/resources/autowhitelistremove.93326/");
-            }
-        });
+        Bukkit.getScheduler().runTaskAsynchronously(this, this::checkForUpdates);
     }
 
     public void loadConfig() {
@@ -71,6 +67,29 @@ public final class AutoWhitelistRemove extends JavaPlugin {
         boolean isCorrect = whitelistCheck.verifyTimeDuration(config.getString("inactive-period"));
         if (!isCorrect) {
             logger.warning("The time duration currently set is not valid! This will break everything!");
+        }
+    }
+
+    public void checkForUpdates() {
+        GitHubReleaseAPI api;
+        try {
+            api = new GitHubReleaseAPI("AutoWhitelistRemove", "hyperdefined");
+        } catch (IOException e) {
+            logger.warning("Unable to check updates!");
+            e.printStackTrace();
+            return;
+        }
+        GitHubRelease current = api.getReleaseByTag(this.getDescription().getVersion());
+        GitHubRelease latest = api.getLatestVersion();
+        if (current == null) {
+            logger.warning("You are running a version that does not exist on GitHub. If you are in a dev environment, you can ignore this. Otherwise, this is a bug!");
+            return;
+        }
+        int buildsBehind = api.getBuildsBehind(current);
+        if (buildsBehind == 0) {
+            logger.info("You are running the latest version.");
+        } else {
+            logger.warning("A new version is available (" + latest.getTagVersion() + ")! You are running version " + current.getTagVersion() + ". You are " + buildsBehind + " version(s) behind.");
         }
     }
 }

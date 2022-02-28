397d4d19428fba78e2b24fa6fd139113a803f518
hyperdefined <contact@hyper.lol>
2022-02-28 12:56:55 -0500

    Update CommandAWR.java

--- src/main/java/lol/hyper/autowhitelistremove/command/CommandAWR.java
+++ src/main/java/lol/hyper/autowhitelistremove/command/CommandAWR.java
@@ -22,6 +22,7 @@ import net.md_5.bungee.api.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -37,7 +38,7 @@ public class CommandAWR implements TabExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (args.length == 1) {
             switch (args[0]) {
                 case "check": {
@@ -109,7 +110,7 @@ public class CommandAWR implements TabExecutor {
     }
 
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
         if (args.length == 2) {
             if (args[0].equalsIgnoreCase("check")) {
                 return Collections.singletonList("confirm");
